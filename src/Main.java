f9cbc3075c087cf051782be75c11f778e762b7e7
“LePeppino” <giuseppe.buccellato@hs-osnabrueck.de>
2020-06-11 16:55:44 +0200

    main implemented, algorithm works, improvements possible

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,11 @@ public class Main {
         File fileTinyFN = new File("res/tinyFN.txt");
         File fileMediumEWG = new File("res/mediumEWG.txt");
 
+        Dijkstra dijkstra = null;
+        Dijkstra dijkstra1 = null;
+        Dijkstra dijkstra2 = null;
+
         Scanner scanner = new Scanner(System.in);
-        Dijkstra dijkstra = new Dijkstra(0);
 
         int fileInput = 0;
         int userInput = 0;
@@ -26,46 +29,49 @@ public class Main {
          * Choose one of the predefined files, because they have
          * the right format for the algorithm to work properly
          * */
-        while(fileInput != exit){
+        while(userInput != exit){
             System.out.println("\nPlease choose a file:" +
                     "\n(1) TinyEWG.txt" +
                     "\n(2) TinyFN.txt" +
                     "\n(3) mediumEWG.txt");
-            fileInput = scanner.nextInt();
+            userInput = scanner.nextInt();
 
-            switch(fileInput){
+            switch(userInput){
                 case 1:
                     System.out.println("File path: " + fileTinyEWG.getAbsolutePath());
                     // create initial graph
                     Scanner reader = new Scanner(new FileReader(fileTinyEWG)).useLocale(Locale.US);
-                    dijkstra.setN(reader.nextInt());
+                    dijkstra = new Dijkstra(reader.nextInt());
                     reader.nextInt();
                     while(reader.hasNext()){
                         dijkstra.addEdge(reader.nextInt(), reader.nextInt(), reader.nextDouble());
                     }
-                    fileInput = exit;
+                    fileInput = 1;
+                    userInput = exit;
                     break;
                 case 2:
                     System.out.println("File path: " + fileTinyFN.getAbsolutePath());
                     // create initial graph
                     Scanner reader1 = new Scanner(new FileReader(fileTinyFN)).useLocale(Locale.US);
-                    dijkstra.setN(reader1.nextInt());
+                    dijkstra1 = new Dijkstra(reader1.nextInt());
                     reader1.nextInt();
                     while(reader1.hasNext()){
-                        dijkstra.addEdge(reader1.nextInt(), reader1.nextInt(), reader1.nextDouble());
+                        dijkstra1.addEdge(reader1.nextInt(), reader1.nextInt(), reader1.nextDouble());
                     }
-                    fileInput = exit;
+                    fileInput = 2;
+                    userInput = exit;
                     break;
                 case 3:
                     System.out.println("File path: " + fileMediumEWG.getAbsolutePath());
                     // create initial graph
                     Scanner reader2 = new Scanner(new FileReader(fileMediumEWG)).useLocale(Locale.US);
-                    dijkstra.setN(reader2.nextInt());
+                    dijkstra2 = new Dijkstra(reader2.nextInt());
                     reader2.nextInt();
                     while(reader2.hasNext()){
-                        dijkstra.addEdge(reader2.nextInt(), reader2.nextInt(), reader2.nextDouble());
+                        dijkstra2.addEdge(reader2.nextInt(), reader2.nextInt(), reader2.nextDouble());
                     }
-                    fileInput = exit;
+                    fileInput = 3;
+                    userInput = exit;
                     break;
                 default:
                     break;
@@ -77,15 +83,35 @@ public class Main {
         * */
         System.out.println("Dijkstra's algorithm (directed graph)");
 
+        userInput = 0;
+
         while(userInput != exit){
             System.out.println("\tSpecify a starting point: ");
             userInput = scanner.nextInt();
-            if(userInput < 0 || userInput > dijkstra.getGraph().size() - 1){
-                System.out.println("Not a valid starting point.");
-                break;
+            if(fileInput == 1){
+                if(userInput < 0 || userInput > dijkstra.getGraph().size() - 1){
+                    System.out.println("Not a valid starting point.");
+                    break;
+                }
+                startInput = userInput;
+                userInput = exit;
             }
-            startInput = userInput;
-            userInput = exit;
+            if(fileInput == 2){
+                if(userInput < 0 || userInput > dijkstra1.getGraph().size() - 1){
+                    System.out.println("Not a valid starting point.");
+                    break;
+                }
+                startInput = userInput;
+                userInput = exit;
+            }
+            if(fileInput == 3){
+                if(userInput < 0 || userInput > dijkstra2.getGraph().size() - 1){
+                    System.out.println("Not a valid starting point.");
+                    break;
+                }
+                startInput = userInput;
+                userInput = exit;
+            }
         }
 
         userInput = 0;
@@ -93,15 +119,42 @@ public class Main {
         while(userInput != exit){
             System.out.println("\tSpecify a destination point: ");
             userInput = scanner.nextInt();
-            if(userInput < 0 || userInput > dijkstra.getGraph().size() - 1){
-                System.out.println("Not a valid ending point.");
-                break;
+            if(fileInput == 1){
+                if(userInput < 0 || userInput > dijkstra.getGraph().size() - 1){
+                    System.out.println("Not a valid ending point.");
+                    break;
+                }
+                endInput = userInput;
+
+                dijkstra.dijkstra(startInput, endInput);
+                System.out.println(dijkstra.reconstructPath(startInput, endInput).toString());
+                userInput = exit;
             }
-            endInput = userInput;
-            userInput = exit;
+            if(fileInput == 2){
+                if(userInput < 0 || userInput > dijkstra1.getGraph().size() - 1){
+                    System.out.println("Not a valid ending point.");
+                    break;
+                }
+                endInput = userInput;
+
+                dijkstra1.dijkstra(startInput, endInput);
+                System.out.println(dijkstra1.reconstructPath(startInput, endInput).toString());
+                userInput = exit;
+            }
+            if(fileInput == 3){
+                if(userInput < 0 || userInput > dijkstra2.getGraph().size() - 1){
+                    System.out.println("Not a valid ending point.");
+                    break;
+                }
+                endInput = userInput;
+
+                dijkstra2.dijkstra(startInput, endInput);
+                System.out.println(dijkstra2.reconstructPath(startInput, endInput).toString());
+                userInput = exit;
+            }
+
         }
 
-        dijkstra.dijkstra(startInput, endInput);
-        System.out.println(dijkstra.reconstructPath(startInput, endInput).toString());
+
     }
 }
